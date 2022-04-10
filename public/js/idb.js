d92aeb424c2fcef54d8b097ed22397a2be708474
Robert World <64225836+sabrtraveler@users.noreply.github.com>
2022-04-10 11:03:57 -0400

    upload transaction data

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,63 +1,63 @@
-// create variable to hold db connection
+// Create var for DB connection 
 let db;
 
-// establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
+// Est. Connection to IndexedDB called budget_tracker and make it version 1
 const request = indexedDB.open('budget_tracker', 1);
 
-// this event will emit if the database version changes
+// This event will happen if the DB version changes 
 request.onupgradeneeded = function(event) {
 
     // save a reference to the database
     const db = event.target.result;
 
-    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
+    // Create table called new transactions and set it to have incrementing primary key of sorts 
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
-// upon a successful
+// upon being successful
 request.onsuccess = function(event) {
 
-    // when db is successfully created with its object store or simply established a connection, save reference to db in global variable
+    // When DB is created successfully then save reference to DB in global variable 
     db = event.target.result;
 
-    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
+    // See if the app is online and if so then run uploadTransaction function to send all local DB data to api 
     if (navigator.onLine) {
         // todo: uploadTransaction();
     }
 };
 
 request.onerror = function(event) {
-    // log error here
+    // this is where we log the error
     console.log(event.target.errorCode);
 };
 
-// This function will be executed if we attempt to submit a new transaction and there's no internet connection
+// this function will run if we try to submit a new transaction but there is no internet connection 
 function saveRecord(record) {
 
-    // open a new transaction with the database with read and write permissions
+    // open new transaction with the database with the DB with read and write permissions 
     const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    // access the object store for `new_transaction`
+    // access the object store for new_transaction 
     const  budgetObjectStore = transaction.objectStore('new_transaction');
 
-    // add record to your store with add method
+    // add record to your store using add method 
     budgetObjectStore.add(record);
 }
 
 function uploadTransaction() {
 
-    // open a transaction on your db
+    // open transaction on your DB 
     const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    // access your object store
+    // Access your object store 
     const budgetObjectStore = transaction.objectStore('new_transaction');
 
-    // get all records from store and set to a variable
+    // Retrieve all records from store and set to a variable 
     const getAll = budgetObjectStore.getAll();
 
     getAll.onsuccess = function() {
 
-        // if there was data in indexedDb's store send it to the api server
+        // If there is data in indexDB;s store then send it to api server 
         if (getAll.result.length > 0) {
             fetch('/api/transaction', {
                 method: 'POST',
@@ -73,13 +73,13 @@ function uploadTransaction() {
                         throw new Error(serverResponse);
                     }
 
-                    // open one more transaction
+                    // Open another transaction 
                     const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-                    // access the new_transaction object store
+                    // Access the new transaction object store 
                     const budgetObjectStore = transaction.objectStore('new_transaction');
 
-                    // clear all items in your store
+                    // Clear the items in the store 
                     budgetObjectStore.clear();
 
                     alert('All saved transactions has been submitted!');
@@ -91,5 +91,5 @@ function uploadTransaction() {
     }
 }
 
-// listen for app coming back online
+// Listen for app coming back online 
 window.addEventListener('online', uploadTransaction);
